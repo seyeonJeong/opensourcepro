10a6e7e916186bb21336a9a986eb7cb8edb8f048
seyeonJeong <81068870+seyeonJeong@users.noreply.github.com>
2021-10-16 16:29:18 +0900

    Add files via upload

---
+++ app1016/prac6_4.java
@@ -0,0 +1,9 @@
+package app1016;
+import base.Shape1016;
+import derived.Circle1016;
+public class prac6_4 {
+	public static void main(String[] args) {
+		Shape1016 shape = new Circle1016();
+		shape.draw();
+	}
+}
