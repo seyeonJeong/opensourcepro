4d2e07346fd1fd8fffd028e47c0ffee00e362c1a
seyeonJeong <81068870+seyeonJeong@users.noreply.github.com>
2021-11-16 21:36:42 +0900

    Add files via upload

---
+++ classprac1112/classprac1.java
@@ -0,0 +1,29 @@
+package classprac1112;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class classprac1 extends JFrame  {
+	public classprac1() {
+		setTitle("Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		btn.addActionListener(new MyActionListener()); // Action 리스너 달기
+		c.add(btn);
+		setSize(350, 150);
+		setVisible(true); }
+		
+		public static void main(String [] args) {
+		new classprac1();
+		}
+		}
+		class MyActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton)e.getSource();
+		if(b.getText().equals("Action"))
+		b.setText("액션");
+		else
+		b.setText("Action");
+		}
+}
